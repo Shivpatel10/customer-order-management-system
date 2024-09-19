8d9831ca91adfa14e10274339c2d25284fea839d
Shiv Patel <shivpatel0204@gmail.com>
2024-09-19 16:25:04 -0400

    Initialize inventory with product objects

--- customer_order_management_system.js
+++ customer_order_management_system.js
@@ -1,2 +1,9 @@
 
 // Task 1: Create an Inventory Array of Product Objects
+let inventory = [
+    {name: "Americano",   price: 6, quanitiy: 4},
+    {name: "Latte",       price: 5, quanitiy: 6},
+    {name: "Cappuccino",  price: 4, quanitiy: 10},
+    {name: "Frappuccino", price: 7, quanitiy: 8}
+];
+console.log(inventory[1]);
