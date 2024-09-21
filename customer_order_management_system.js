1214053a4a498afed19ecc0ef676849c4b223818
Shiv Patel <shivpatel0204@gmail.com>
2024-09-20 23:32:58 -0400

    Create calculateOrderTotal function

--- customer_order_management_system.js
+++ customer_order_management_system.js
@@ -36,3 +36,15 @@ function placeOrder (customerName, itemsOrdered) {
                 });
     console.log(`${customerName} your order has been placed!`) // on the customer side it will output a message telling them the order has been placed
 }
+
+// Task 4: Create a Function to Calculate Total for an Order
+
+function completeOrder (order, total = 0) { // added a parameter of total = 0 to start it at a value of 0
+    order.items.forEach( item => { // creating a forEach loop so it will go to the orders array and check the items 
+        let coffee = inventory.find( product => product.name === item.name); // reusing coffee like i did before
+        if (coffee) {
+            total += coffee.price * item.quantity; // this will start with total at 0 and add the what ever coffee price is in terms of how much is ordered/
+        }
+    });
+    return total;
+};
